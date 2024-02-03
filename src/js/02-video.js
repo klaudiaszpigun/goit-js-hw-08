c1a53989445f1b87d4e6627ff94d7dad69b22875
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-02-03 19:09:36 +0100

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,34 @@
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const iframe = document.querySelector('#vimeo-player');
+// zainicjowanie biblioteki vimeo
+// zmienna player to video
+const player = new Player(iframe);
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+// zmienna savedtime to pobrane dane z localStorage, są to pliki posiadające pliki .JSON, więc zmienna posiada string
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+/* if (savedTime != "" ) {
+    savedTime = parseFloat(savedTime);
+  }
+  else {
+    savedTime = 0;
+  }
+*/
+const initialTime = savedTime ? parseFloat(savedTime) : 0;
+
+const saveCurrentTime = time => {
+  localStorage.setItem('videoplayer-current-time', time.toString());
+};
+
+player.setCurrentTime(initialTime);
+
+// Dodaj nasłuchiwanie zdarzenia timeupdate z użyciem lodash.throttle
+player.on(
+  'timeupdate',
+  throttle(data => {
+    const currentTime = data.seconds;
+    saveCurrentTime(currentTime);
+  }, 1000)
+); // 1000 milisekund = 1 sekunda
