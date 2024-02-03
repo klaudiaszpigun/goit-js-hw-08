85d1984d3e38e83390f32281f867197a2b30d2e7
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-02-03 18:10:17 +0000

    Deploying to gh-pages from @ klaudiaszpigun/goit-js-hw-08@c1a53989445f1b87d4e6627ff94d7dad69b22875 🚀

--- 02-video.5f5a9ebd.js
+++
@@ -1,2 +0,0 @@
-!function(){let e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",function(){console.log("played the video!")}),o.getVideoTitle().then(function(e){console.log("title:",e)})}();
-//# sourceMappingURL=02-video.5f5a9ebd.js.map
